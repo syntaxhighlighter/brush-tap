c8648a499cf2bda6e478d1049a746eb3f38bd01e
Alex Gorbatchev <alex.gorbatchev@gmail.com>
2015-11-20 10:26:49 -0800

    Updates for 2015
    * Removes CoffeeScript
    * Removes Grunt
    * Removes Karma
    * Uses Babel

---
+++ brush.js
@@ -0,0 +1,52 @@
+/**
+ * SyntaxHighlighter
+ * http://alexgorbatchev.com/SyntaxHighlighter
+ *
+ * @license
+ * Dual licensed under the MIT and GPL licenses.
+ */
+var BrushBase = require('brush-base');
+var regexLib = require('regex-lib');
+
+function Brush() {
+  // Contributed by Chad Granum
+  this.regexList = [
+            // Plan
+    {
+      regex: new RegExp('^1..\\d+', 'gm'),
+      css: 'plain bold italic'
+    },
+    {
+      regex: new RegExp('^ok( \\d+)?', 'gm'),
+      css: 'keyword'
+    },
+    {
+      regex: new RegExp('^not ok( \\d+)?', 'gm'),
+      css: 'color3 bold'
+    },
+    {
+      regex: new RegExp('(?!^\\s*)#.*$', 'gm'),
+      css: 'variable bold'
+    },
+    {
+      regex: new RegExp('^#.*$', 'gm'),
+      css: 'comments bold'
+    },
+    {
+      regex: new RegExp('^(?!(not )?ok)[^1].*$', 'gm'),
+      css: 'comments'
+    },
+    {
+      regex: regexLib.doubleQuotedString,
+      css: 'string'
+    },
+    {
+      regex: regexLib.singleQuotedString,
+      css: 'string'
+    },
+        ];
+}
+
+Brush.prototype = new BrushBase();
+Brush.aliases = ['tap', 'Tap', 'TAP'];
+module.exports = Brush;
